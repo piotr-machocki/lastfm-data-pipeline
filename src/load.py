57972575c9f09debf6057bd64541cf462928c1ad
piotr-machocki <piotrmachocki1@gmail.com>
2026-08-18 13:36:24 +0200

    Parse database port as integer

--- src/load.py
+++ src/load.py
@@ -10,7 +10,7 @@ from config import VALIDATED_CSV
 load_dotenv()
 
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT", "5432")
+DB_PORT = int(os.getenv("DB_PORT", "5432"))
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
