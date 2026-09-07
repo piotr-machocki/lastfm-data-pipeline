72c1d0dd890b3003a557ddd6cac9c9dc9176ee4a
piotr-machocki <piotrmachocki1@gmail.com>
2026-09-07 21:10:44 +0200

    Separate Docker setup from pipeline execution

--- scripts/docker-setup.sh
+++ scripts/docker-setup.sh
@@ -5,7 +5,5 @@ python3 scripts/setup_timezone.py
 
 # Docker Compose reads .env automatically for ${VAR} substitution,
 # so DB_TIMEZONE written above is picked up by db's environment block.
-docker compose up --build
-
-# TODO: once sql/views.sql is implemented, apply it here, e.g.:
-# docker compose exec -T db psql -U "$DB_USER" -d "$DB_NAME" -f - < sql/views.sql
+docker compose build pipeline
+docker compose up -d db
